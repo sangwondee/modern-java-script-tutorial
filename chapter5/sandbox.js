2738fec1759a97571bd16497c7ba3cd7e9ff3c6b
sangwondee <sangwondee@gmail.com>
2020-09-16 09:54:22 +0700

    Lean Object literals and Math Object

--- chapter5/sandbox.js
+++ chapter5/sandbox.js
@@ -1,24 +1,47 @@
-// Object literals
+// Math object
 
-let user = {
-    name: "Wichan Sangwondee",
-    age: 30,
-    email: 'sangwondee@gmail.com',
-    location: 'thailand',
-    blogs: [
-        'why mac & cheese rules',
-        '10 thing you never not to js'
-    ],
-    logIn: () => {
-        console.log('this is login with arrow function');
-    },
-    logOut: () => {
-        console.log('this is log out with arrow function');
-    },
-    logBlogs: () => {
-        console.log('this is logBlogs');
-    }
-}
+// คืทอการใช้งาน objects math ที่มีอยู่ใน javascripts
+console.log(Math);
+console.log(Math.PI + ' => this is pi value');
+console.log(Math.E);
+
+const area = 7.7;
+
+console.log(Math.round(area)); // ปัดค่าขึ้นปัดค่าลง
+console.log(Math.floor(area)); // เอาค่าทศนิยม
+console.log(Math.ceil(area)); //  ปัดค่าขึ้น
+console.log(Math.trunc(area)); // ตัดค่่าเอาเฉพาะตัวเลข
+
+// Object literals
+// const blogs = [
+//     { title : 'why mac & cheet rules', likes: 30 },
+//     { title : '10 think to make with marmite', likes: 50 }
+// ];
+
+// let user = {
+//     name: "Wichan Sangwondee",
+//     age: 30,
+//     email: 'sangwondee@gmail.com',
+//     location: 'thailand',
+//     blogs: [
+//         { title : 'why mac & cheet rules', likes: 30 },
+//         { title : '10 think to make with marmite', likes: 50 }
+//     ],
+//     logIn () {
+//         // console.log('this is login with arrow function');
+//     },
+//     logOut() {
+//         // console.log('this is log out with arrow function');
+//     },
+//     logBlogs() {
+//         // console.log('this is logBlogs');
+//         // console.log('this user has written the following blogs');
+//         // console.log(this);
+//         this.blogs.forEach(blog => {
+//             console.log(blog.title, blog.likes);
+//         });
+//     }
+// }
 
 // dot notation คือการเข้าถึง object โดยการใช้ dot
 // console.log(user);
@@ -34,10 +57,11 @@ let user = {
 // console.log(user)
 
 // การเรียกใช้ methods ใน object (user)
-user.logIn();
-user.logOut();
+// user.logIn();
+// user.logOut();
+// user.logBlogs();
 
-const name = 'wichan sangwondee';
+// const name = 'wichan sangwondee';
 
-name.toUpperCase();
-console.log(name);
+// name.toUpperCase();
+// console.log(name);
