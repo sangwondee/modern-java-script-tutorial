4a2b11bd6dc4af37c562ea2b01966d3310cd4826
sangwondee <sangwondee@gmail.com>
2020-09-22 18:00:20 +0700

    learning get elementById, getElementByClassName, getElementsByTagName

--- chapter6/sandbox.js
+++ chapter6/sandbox.js
@@ -11,9 +11,24 @@
 
 // querySelectorAll => คือ การเลือกเอา element selector โดยต้องมันจะรับ tag eletemt ด้วย.
 
-const params = document.querySelectorAll('p'); // จะออกมาเป็น NodeList ซึ่งเราสามารถใช้ตัวแปรแบบ array ได้
+// const params = document.querySelectorAll('p'); // จะออกมาเป็น NodeList ซึ่งเราสามารถใช้ตัวแปรแบบ array ได้
 
-params.forEach(item => {
-    console.log(item);
-});
+// params.forEach(item => {
+//     console.log(item);
+// });
 // console.log(params);
+
+// Get an element by id
+const title = document.getElementById('page-title'); //ถ้าเป็น getElementById เราไม่จำเป็นต้องมี # เพื่อให้ได้ element
+// console.log(title);
+
+// Get elements by their class name
+const errors = document.getElementsByClassName('error');
+// ค่าที่ได้ออกมาจะเป็น HTMLCollection เป็น arrays แต่เราไม่สามารถใช้ foreach loops กับมันได้.
+// console.log(errors[0]);
+// console.log(errors);
+
+// Get elements by their tag name
+const paras = document.getElementsByTagName('p');
+// จะหา <p> ทั้งหมดที่อยู่ในหน้านั้นแล้วทำการแสดงออกมา
+// console.log(paras);
