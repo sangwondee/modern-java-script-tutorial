5d9bed0dbc9a9b9c995a31dac71da2a768d1133a
sangwondee <sangwondee@gmail.com>
2020-09-22 13:29:30 +0700

    chapter 6

--- chapter6/sandbox.js
+++ chapter6/sandbox.js
@@ -1,3 +1,19 @@
 // ***สำคัญมาก***
-// Document Object Model (DOM) เป็นการเข้าถึงแบบ node (binary tree) โดยจะเข้าถึงเป็น level ไปเลื่อยๆ 
+// Document Object Model (DOM) เป็นการเข้าถึงแบบ node (binary tree) โดยจะเข้าถึงเป็น level ไปเลื่อยๆ
 
+// const para = document.querySelector('p'); // เลือกเอา <p> โดยจะเลือกเอาแต่ตัวแรกที่มันเจอ.
+
+// const para = document.querySelector('.error'); // ถ้าใส่ . จะเลือกเอาชื่อ class แรกของชื่อที่มันเจอ
+
+// const para = document.querySelector('div.error'); // ถ้าใส่ . เราจะสามารสเข้าไปถึง class นั้นๆ เพราะทำงานเป็นแบบ object
+
+// console.log(para);
+
+// querySelectorAll => คือ การเลือกเอา element selector โดยต้องมันจะรับ tag eletemt ด้วย.
+
+const params = document.querySelectorAll('p'); // จะออกมาเป็น NodeList ซึ่งเราสามารถใช้ตัวแปรแบบ array ได้
+
+params.forEach(item => {
+    console.log(item);
+});
+// console.log(params);
