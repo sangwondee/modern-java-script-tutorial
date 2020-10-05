0fc3894c73501819bfa51b658612e10b57180b5f
sangwondee <sangwondee@gmail.com>
2020-10-05 17:44:52 +0700

    Keyboard Event (2)

--- chapter7/sandbox.js
+++ chapter7/sandbox.js
@@ -1,17 +1,25 @@
 //Basic Form validattion
-
 const form = document.querySelector('.signup-form');
-const feedback = document.querySelector('.feedback')
+const feedback = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-z]{6,12}$/;
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
+// form.addEventListener('submit', e => {
+//     e.preventDefault();
 
-    const username = form.username.value;
-    const usernamePattern = /^[a-zA-z]{6,12}$/;
+//     const username = form.username.value;
 
-    if (usernamePattern.test(username)) {
-        feedback.textContent = 'that username is valid !';
+//     if (usernamePattern.test(username)) {
+//         feedback.textContent = 'that username is valid !';
+//     } else {
+//         feedback.textContent = 'username must contain letters only & be between 6 to 12';
+//     }
+// });
+
+form.username.addEventListener('keyup', e => {
+    if (usernamePattern.test(e.target.value)) {
+        // setAttribute คือ set ค่าของ class นั้นๆ ให้ success
+        form.username.setAttribute('class', 'success');
     } else {
-        feedback.textContent = 'username must contain letters only & be between 6 to 12';
+        form.username.setAttribute('class', 'error');
     }
 });
