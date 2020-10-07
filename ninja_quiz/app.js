a46d9f9ce017c1f6939b24df074944e8f5c1dbdc
sangwondee <sangwondee@gmail.com>
2020-10-07 14:06:53 +0700

    Add if short hand to Animation

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -21,17 +21,11 @@ form.addEventListener('submit', e => {
         scrollTo(0,0);
         // result.querySelector('span').textContent = `${score}%`;
         result.classList.remove('d-none');
-
         // เป็นการ set Animations เวลาตัวเลขค่อยเพิ่มขึ้นมา.
         let output = 0;
         const timer = setInterval(() => {
             result.querySelector('span').textContent = `${output}%`;
-
-            if (output === score) {
-                clearInterval(timer);
-            } else {
-                output++;
-            }
+            output === score ? clearInterval(timer) : output++;
         }, 10);
     }
 })
