724429834a223b51e3fc181a75a471b2261a7b65
sangwondee <sangwondee@gmail.com>
2020-10-12 18:05:21 +0700

    Making HTTP Request (XHR)

--- chapter12/app.js
+++ chapter12/app.js
@@ -1,10 +1,16 @@
 // Asynchronous Javascrpit (Start now and finish later) คือการทำงานแบบไม่่ต้องรอ process อื่นก่อนหน้านี้ทำงานเสร็จทำ
-console.log(1)
-console.log(2)
+// console.log(1)
+// console.log(2)
 
-setInterval(() => {
-    console.log('callback function fired !!!');
-}, 2000);
+// setInterval(() => {
+//     console.log('callback function fired !!!');
+// }, 2000);
 
-console.log(3)
-console.log(4)
+// console.log(3)
+// console.log(4)
+
+// HTTP Requests คือ การส่งคำขอไปยังเชิิฟเวอร์เพื่อต้องการข้อมูล โดยจากฝั่ง Client ไปยังฝั่ง Server เพื่อให้ส่งข้อมูลที่ต้องการกลับมา เช่น Client
+//     ต้องการ หน้า home.html ก็จะส่งคำขอ GET /home -> ไปที่เชิฟเวอร์ แล้วตอบกลับเป็น home.html ออกมา
+
+// ใช้อันนี้ https://jsonplaceholder.typicode.com/ -> website ทดลอง http requests
+
