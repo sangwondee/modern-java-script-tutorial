1b755c351b10392db110edda17fcb113740682a5
sangwondee <sangwondee@gmail.com>
2020-10-12 13:25:47 +0700

    Timestamps & Comparisons

--- chapter11/app.js
+++ chapter11/app.js
@@ -1,24 +1,48 @@
 // DateTime is a object (Reference Type)
+// const now = new Date;
 
-const now = new Date;
+// console.log(now);
+// console.log(typeof now); // object
 
-console.log(now);
-console.log(typeof now); // object
+// // year, months, day, times
+// console.log('getFullYear:' , now.getFullYear());
+// console.log('getMonth:' , now.getMonth());
+// console.log('getDate:' , now.getDate());
+// console.log('getDay:' , now.getDay());
+// console.log('getHours:' , now.getHours());
+// console.log('getMinutes:' , now.getMinutes());
+// console.log('getSeconds:' , now.getSeconds());
 
-// year, months, day, times
-console.log('getFullYear:' , now.getFullYear());
-console.log('getMonth:' , now.getMonth());
-console.log('getDate:' , now.getDate());
-console.log('getDay:' , now.getDay());
-console.log('getHours:' , now.getHours());
-console.log('getMinutes:' , now.getMinutes());
-console.log('getSeconds:' , now.getSeconds());
+// // timestamps
+// console.log('timestamp', now.getTime());
+
+// //date strings
+// console.log(now.toDateString());
+// console.log(now.toTimeString());
+// console.log(now.toLocaleDateString());
+
 
 // timestamps
-console.log('timestamp', now.getTime());
+// เราสามารถ ใส่ค่าวันที่ที่เป็นตัวอักษรเข้าไปได้เลย
+const before = new Date('February 1 2019 7:30:59');
+const now = new Date;
 
-//date strings
-console.log(now.toDateString());
-console.log(now.toTimeString());
-console.log(now.toLocaleDateString());
+console.log(now.getTime(), before.getTime());
+
+// หาค่า เวลาที่ต่างกันจาก timestamp
+const diff = now.getTime() - before.getTime();
+console.log(diff);
+
+const mins = Math.round(diff / 1000 / 60); // เพราะ 1000 ms เท่ากับ 1 นาที
+const hours = Math.round(mins / 60);
+const days = Math.round(hours / 24);
+
+console.log(mins, hours, days);
+console.log(`the blog was written ${days} ago`);
+
+// converting timestamps into date objects
+const timestamp = 1675938474990;
+
+console.log(new Date(timestamp));
+
 
