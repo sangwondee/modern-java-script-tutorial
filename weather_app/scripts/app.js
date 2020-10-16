0231267076f20b63f081e9137b71e59033281a07
sangwondee <sangwondee@gmail.com>
2020-10-16 14:33:49 +0700

    Destructuring

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -3,8 +3,10 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 
 const updateUI = (data) => {
-    const cityDetail = data.cityDetail;
-    const weather = data.weather;
+    // Destructuring Object
+    // อ่านเพิ่มเติมจากอันนี้
+    // => https://medium.com/@brightsongpat/%E0%B8%A1%E0%B8%B2%E0%B8%A3%E0%B8%B9%E0%B9%89%E0%B8%88%E0%B8%B1%E0%B8%81%E0%B8%81%E0%B8%B2%E0%B8%A3-destructuring-object%E0%B9%83%E0%B8%99-es6-%E0%B8%81%E0%B8%B1%E0%B8%99%E0%B9%80%E0%B8%97%E0%B8%AD%E0%B8%B0-470018a0f2bd
+    const { cityDetail, weather } = data;
 
     // update Detail template
     details.innerHTML = `
