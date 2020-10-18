505511b195402dc53cadf331d2bbfcc6115c769f
sangwondee <sangwondee@gmail.com>
2020-10-18 23:50:40 +0700

    Storing & Getting Data

---
+++ weather_app/chapter14/sandbox.js
@@ -0,0 +1,19 @@
+// local storage คือ การเก็บ
+
+
+// stoage data in local storage
+localStorage.setItem('name', 'mario');
+localStorage.setItem('age', 50);
+
+// get data from local storage
+let name = localStorage.getItem('name');
+let age = localStorage.getItem('age');
+
+// updateing data
+localStorage.setItem('name', 'luigi');
+localStorage.age = 40;
+
+name = localStorage.getItem('name')
+age = localStorage.getItem('age')
+console.log(name, age);
+
