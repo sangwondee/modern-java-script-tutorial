dec24cfc08fd6dc62c4b2439390d5f10dcc62bc9
wichans <wichans@investree.co.th>
2020-12-09 18:48:18 +0700

    Write admin class

--- chapter15/app.js
+++ chapter15/app.js
@@ -96,10 +96,33 @@ class User {
     }
 }
 
+
+//
+
+class Admin extends User {
+    deleteUser(user) {
+        users = users.filter((u) => {
+            return u.username !== user.username
+        })
+    }
+}
+
 const userOne = new User('mario', 'mario@thenetninja.co.uk')
 const userTwo = new User('luigi', 'luigi@thenetninja.co.uk')
+const userThree = new Admin('shaun', 'shaun@thenetninja.co.uk');
 
-console.log(userOne)
-console.log(userTwo)
+// console.log(userOne)
+// console.log(userTwo)
 
-userOne.login().incScore().incScore().logout();
+// userOne.login().incScore().incScore().logout();
+console.log(userOne,userTwo,userThree);
+
+let users = [userOne, userTwo, userThree];
+
+console.log(users);
+
+
+userThree.deleteUser(userTwo);
+
+
+
