7dbb9c98575b76ea39e0c2915a934ddfbda91869
wichans <wichan@investree.co.th>
2021-03-15 18:45:59 +0700

    Updating the Room

--- chat_project/scripts/app.js
+++ chat_project/scripts/app.js
@@ -3,6 +3,7 @@ const chatList = document.querySelector('.chat-list')
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
 const updateMessg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
 
 
 // add a new chat
@@ -28,10 +29,21 @@ newNameForm.addEventListener('submit', e => {
     setTimeout(() => updateMessg.innerText = '' ,3000);
 });
 
+// update the chat rooms
+rooms.addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') {
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(chat => chatUI.render(chat));
+    }
+});
+
 
 // check local storage for a name
 const username = localStorage.username ? localStorage.username : 'anon';
 
+
+// Start here !!
 // class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', username);
