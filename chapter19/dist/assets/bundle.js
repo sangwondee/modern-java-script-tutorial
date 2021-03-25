de7c847428b7a3b2a190d5c58243caa9a3be0c88
sangwondee <sangwondee@gmail.com>
2021-03-25 17:32:43 +0700

    not gitignore file need nothing important

---
+++ chapter19/dist/assets/bundle.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var greet = function greet(name) {
+  console.log("hello ".concat(name));
+};
+
+greet('mario friday');
+greet('luigi tuesday');
+greet('link monday');
+greet('link sunday');
+greet('link wensday test');
---
+++ chapter19/src/index.js
@@ -0,0 +1,10 @@
+const greet = (name) => {
+    console.log(`hello ${name}`);
+}
+
+greet('mario friday');
+greet('luigi tuesday');
+greet('link monday');
+greet('link sunday');
+greet('link wensday test');
+
