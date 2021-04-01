806a386f1250769ee7cf689f4ac1f08a35991155
wichans <wichan@investree.co.th>
2021-04-02 00:09:00 +0700

    Snackbars

--- ui-lib-project/src/index.js
+++ ui-lib-project/src/index.js
@@ -1,6 +1,7 @@
 import Tooltip from './ninja-ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
 import Tabs from './ninja-ui/tabs';
+import Snackbar from './ninja-ui/snackbar';
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -16,3 +17,12 @@ dropdowns.forEach(dropdown => {
 
 const tabs = new Tabs(document.querySelector('.tabs'));
 tabs.init();
+
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    snackbar.show('you click me :)');
+});
