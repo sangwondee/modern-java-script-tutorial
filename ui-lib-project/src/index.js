b800bc9436d59c218055b6fcd4d1e5c3e301341f
wichans <wichan@investree.co.th>
2021-04-01 22:46:59 +0700

    Tabbed Content

--- ui-lib-project/src/index.js
+++ ui-lib-project/src/index.js
@@ -1,5 +1,6 @@
 import Tooltip from './ninja-ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
+import Tabs from './ninja-ui/tabs';
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -12,3 +13,6 @@ dropdowns.forEach(dropdown => {
     const instance = new Dropdown(dropdown);
     instance.init();
 });
+
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
