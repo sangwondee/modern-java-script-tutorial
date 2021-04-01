fd7a1100e3103c1bf48d9ae1cc5ffc8725c7378f
wichans <wichans@investree.co.th>
2021-04-01 14:20:53 +0700

    Tooltip

--- ui-lib-project/src/index.js
+++ ui-lib-project/src/index.js
@@ -1,2 +1,6 @@
-import './test.css';
-console.log('test');
+import Tooltip from './ninja-ui/tooltip';
+
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+
+tooltip.init();
